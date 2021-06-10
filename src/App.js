75a5acb8a8c2f304d69e50cb99b94ada2c22b97f
taeeun <taeeun@taeeunui-MacBookPro.local>
2021-06-10 12:25:02 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -26,14 +26,14 @@ function App() {
       type: 'column',
       data: [2.8, 2.5, 3.5, 3.2, 2.8, 3.3, 2.9],
       tooltip: {
-        pointFormat: '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y:.1f} mm</b> '
+        pointFormat: '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y}</b> '
       }
     }, {
       name: '표준편차',
       type: 'errorbar',
       data: [[0.6, 5.0], [0.7, 4.3], [1.5, 5.5], [1.2, 5.2], [0.5, 5.1], [1.2, 5.4], [1.1, 4.7]],
       tooltip: {
-        pointFormat: '({series.name}: {point.low}-{point.high} mm)<br/>'
+        pointFormat: '({series.name}: {point.low}-{point.high})<br/>'
       }
     }]
   }
@@ -52,7 +52,7 @@ function App() {
     tooltip: {
       headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
       pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        '<td style="padding:0"><b>{point.y}</b></td></tr>',
       footerFormat: '</table>',
       shared: true,
       useHTML: true
@@ -93,7 +93,7 @@ function App() {
       type: 'column',
       data: [4659, 3618, 5811, 6464, 3648, 4006, 4523],
       tooltip: {
-        pointFormat: '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y:.1f} mm</b> '
+        pointFormat: '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y}</b> '
       },
     }, {
       name: '표준편차',
